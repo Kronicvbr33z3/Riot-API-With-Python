734d02327ba2d84774d61d45f88fbe72343da609
Kronicvbr33z3 <gonzalezdavidr22@gmail.com>
2017-11-06 16:07:31 -0800

    Added Functinalitly

--- Bot.py
+++ Bot.py
@@ -1,19 +1,86 @@
-import urllib
+
 import requests
 """
 Tool that will provide information about a summoner in python
 """
-
+RIOT_API_KEY = 'RGAPI-f1784676-86a3-4132-9990-c27271266577'
+status = "OK"
 print("League of Legends Tool!")
 print("Made by Kronic Vayne, Version: 0.1")
-
+print("----------------------------------")
 #Getting Summoner Name(Replacing Spaces with %20 for url usage)
+def analyzeMatch(matchId):
+    print("Match ID: " + str(matchId))
+    MatchURL = 'https://na1.api.riotgames.com/lol/match/v3/matches/' + str(matchId) + '?api_key=' + RIOT_API_KEY
+    match_json = requests.get(MatchURL).json()
+    foundPlayer = False
+    
+    i = 0
+    participantId = 0
+    while i >= 10:
+      
+      tempPlayer = match_json['participantIdentities'][i]['currentAccountId']
+      print(tempPlayer)
+      i += 1 
+      """
+      if tempPlayer == Summoner_Raw:
+        participantId =  match_json['participantIdentities'][i]['participantId']
+        
+        foundPlayer = True
+      else:
+        i += 1
+      """
+    
 
-Summoner_Raw = input('Enter Summoner Name: ')
-Summoner = Summoner_Raw.replace(' ', '%20')
 
-SummonerName = 'https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/'+ Summoner +'?api_key=RGAPI-888e39fd-576c-4d22-83dd-2678186ef630'
+def matchHistory(id):
+    print("Showing Match History for " + Summoner_Raw)
+    MatchHistory = 'https://na1.api.riotgames.com/lol/match/v3/matchlists/by-account/' + str(id) +'/recent?api_key=' + RIOT_API_KEY
+    match_history_json = requests.get(MatchHistory).json()
+    mMatches = match_history_json['endIndex']
+    cMatches = match_history_json['startIndex']
+    
+    while cMatches <= 5:
+        tempChampId = match_history_json['matches'][cMatches]['champion']
+        
+        tempChampionURL = 'https://na1.api.riotgames.com/lol/static-data/v3/champions/' + str(tempChampId) + '?locale=en_US&api_key=' + RIOT_API_KEY
+        tempChamp_json = requests.get(tempChampionURL).json()
+        try:
+            champ = tempChamp_json['name']
+        except:
+            print("Riot API Error")
+            break
+        print("(" + str(cMatches) + ") Champion: " + champ)
+        cMatches += 1
+    matchChoice = int(input("Choose which match you want to analyze: "))
+    matchId = match_history_json['matches'][matchChoice]['gameId']
 
 
-json_data_summonerID = requests.get(SummonerName).json()
-summonerId = json_data_summonerID['id']
+
+    analyzeMatch(matchId)
+        
+    
+while True:
+
+    Summoner_Raw = input('Enter Summoner Name: ')
+    Summoner = Summoner_Raw.replace(' ', '%20')
+    if Summoner_Raw == "quit" or Summoner_Raw == "q":
+        print("Quitting...")
+        break
+    SummonerName = 'https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/'+ Summoner +'?api_key=' + RIOT_API_KEY
+
+    json_data_summonerID = requests.get(SummonerName).json()
+    
+    try:
+        summonerId = json_data_summonerID['id']
+        accountId = json_data_summonerID['accountId']
+   
+    except: 
+        print("No Summoner Found!")
+        status = json_data_summonerID['status']['message']
+    if status == "OK":
+        matchHistory(accountId)
+        break
+    else:
+        status = "OK"
+        print()
